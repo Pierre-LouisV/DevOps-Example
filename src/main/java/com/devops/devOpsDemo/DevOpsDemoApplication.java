e8f2395077948b6a061dd682795228a5e866b390
virey <pierrelouis.virey.etu@univ-lille.fr>
2021-03-21 14:09:55 +0100

    feat: Ajout d'une message d'accueil perso

--- src/main/java/com/devops/devOpsDemo/DevOpsDemoApplication.java
+++ src/main/java/com/devops/devOpsDemo/DevOpsDemoApplication.java
@@ -2,12 +2,16 @@ package com.devops.devOpsDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DevOpsDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevOpsDemoApplication.class, args);
-	}
+		public static void main(String[] args) {
+			ApplicationContext ctx = SpringApplication.run(DevOpsDemoApplication.class, args);
+
+			System.out.println("Started application.");
+		}
+
 
 }
